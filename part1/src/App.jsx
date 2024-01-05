9e3b4f7d91d2983b5680a559d9f7022e558997aa
Aniwyn <kanashiiguree@gmail.com>
2024-01-04 22:01:17 -0300

    Exercise 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,51 @@
-const Hello = (props) => { 
+const Header = (prpos) => {
   return(
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+    <>
+      <h1>{prpos.course}</h1>
+      
+    </>
+  )
+}
+
+const Content = (prpos) => {
+  return(
+    <>
+      <p>
+        {prpos.part1} {prpos.exercises1}
+      </p>
+      <p>
+        {prpos.part2} {prpos.exercises2}
+      </p>
+      <p>
+        {prpos.part3} {prpos.exercises3}
+      </p>
+    </>
+  )
+}
+
+const Total = () => {
+  return(
+    <>
+      <h1>{prpos.course}</h1>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </>
   )
 }
 
 const App = () => {
-  return(
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Jose" age="20"/>
-      <Hello name="Jorge" age={20 + 10}/>
+      <Header course={ course }/>
+      <Content part1={ part1 } exercises1={ exercises1 } part2={ part2 } exercises2={ exercises2 } part3={ part3 } exercises3={ exercises3 }/>
+      <Total exercises1={ exercises1 } exercises2={ exercises2 } exercises3={ exercises3 }/>
     </div>
   )
 }
