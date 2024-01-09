caca625a644da6d7eb1f6ee8dfab6eec7c313b72
Aniwyn <kanashiiguree@gmail.com>
2024-01-09 00:06:03 -0300

    Exercise 2.14

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,16 +2,51 @@ import { useState, useEffect } from 'react'
 
 import axios from 'axios'
 import Countries from './components/Countries'
+import KEY from '../KEY'
+// const KEY = ''
 
 
 function App() {
   const [ countries, setCountries ] = useState([])
+  const [ filteredCountries, setFilteredCountries ] = useState([])
   const [ search, setSearch ] = useState('')
+  const [ weather, setWeather ] = useState({})
 
   useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all')
-      .then(response => setCountries(response.data))
+      .then(response => {
+        setCountries(response.data)
+        setFilteredCountries(response.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    const expression = new RegExp(`.*${search}.*`, 'i')
+    const filtered = countries.filter(countrie => expression.test(countrie.name.common))
+    setFilteredCountries(filtered)
+    if (filtered.length === 10000000) {
+      console.log(`\nhttp://api.weatherstack.com/current?access_key=${KEY}&query=${filtered[0].name.common}\n`)
+      axios
+        .get(`http://api.weatherstack.com/current?access_key=${KEY}&query=${filtered[0].name.common}`)
+        .then(r => {
+          console.log(r)
+          return(r)
+        })
+        .then(response => setWeather(response))
+        .catch(error => console.error(error))
+    }
+  }, [search])
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${KEY}&query=Argentina`)
+      .then(r => {
+        console.log(r)
+        return(r)
+      })
+      .then(response => setWeather(response))
+      .catch(error => console.error(error))
   }, [])
 
   const handleSearchChange = (event) => setSearch(event.target.value)
@@ -28,7 +63,11 @@ function App() {
   return (
     <div>
       <div>find countries <input value={search} onChange={handleSearchChange}></input></div>
-      <Countries countries={countries} search={search} details={viewDetails}></Countries>
+      <Countries
+        countries={filteredCountries}
+        details={viewDetails}
+        weather={weather}>
+      </Countries>
     </div>
   )
 }
--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,47 +1,54 @@
-const Countrie = ({ countrie }) => {
+const Countrie = ({ countrie, weather }) => {
     const languages = Object.entries(countrie.languages)
     return(
-      <div>
-        <h1>{countrie.name.common}</h1>
         <div>
-          <p>Capital: {countrie.capital[0]}</p>
-          <p>Population: {countrie.population}</p>
+            <h1>{countrie.name.common}</h1>
+            <div>
+                <p>Capital: {countrie.capital[0]}</p>
+                <p>Population: {countrie.population}</p>
+            </div>
+            <div>
+                <h2>Languages</h2>
+                <ul>
+                    {languages.map(language => <li key={language[0]}>{language[1]}</li>)}
+                </ul>
+            </div>
+            <img src={countrie.flags.png} width={100}/>
+            <div>
+                <h2>Weather in {weather.data.location.name}</h2>
+                <p><strong>Temperature: </strong>{weather.data.current.temperature} celcius</p>
+                <img src={weather.data.current.weather_icons[0]} width={100}/>
+                <p><strong>Wind: </strong>{weather.data.current.wind_speed} mph direction {weather.data.current.wind_dir}</p>
+            </div>
         </div>
-        <div>
-          <h2>Languages</h2>
-          <ul>
-            {languages.map(language => <li key={language[0]}>{language[1]}</li>)}
-          </ul>
-        </div>
-        <img src={countrie.flags.png} width={100}/>
-      </div>
     )
 }
 
 
-const Countries = ({ countries, search, details }) => {
-    const expression = new RegExp(`.*${search}.*`, 'i')
-    const filteredCountries = countries.filter(countrie => expression.test(countrie.name.common))
-  
-    if (filteredCountries.length >= 10) {
+const Countries = ({ countries, details, weather }) => {
+    if (countries.length >= 10) {
       return(<div>Too many matches, specify another filter</div>)
     }
-  
-    if (filteredCountries.length === 1) {
-      return(
-        <Countrie countrie={filteredCountries[0]}></Countrie>
-      )
+
+    if (countries.length === 0) {
+        return(<div>No results</div>)
     }
   
+    if (countries.length === 1) {
+      return(
+        <Countrie countrie={countries[0]} weather={weather}></Countrie>
+      )
+    }
+    
     return(
       <div>
-        {filteredCountries.map(countrie => {
-          return(
-            <form key={countrie.name.common} onSubmit={details} name={countrie.name.common}>
-              <span>{countrie.name.common} </span>
-              <button type="submit">show</button>
-            </form>
-          )
+        {countries.map(countrie => {
+            return(
+                <form key={countrie.name.common} onSubmit={details} name={countrie.name.common}>
+                    <span>{countrie.name.common} </span>
+                    <button type="submit">show</button>
+                </form>
+            )
         })}
       </div>
     )
